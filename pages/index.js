8b9295af2f831cbc23965cedefd41969302b58fc
John <58703369+johnsistem@users.noreply.github.com>
2023-02-24 08:11:21 -0600

    cambio en rama main

--- pages/index.js
+++ pages/index.js
@@ -45,8 +45,8 @@ const usersList = useSelector((state) => state.usersData);
 						<a href="https://nextjs.org/docs" className={styles.card}>
 							<h2>Documentation &rarr;</h2>
 							<p>
-								Find in-depth information about Next.js features and
-								API.
+								{/* Find in-depth information about Next.js features and
+								API. */}
 							</p>
 						</a>
 					</Link>
