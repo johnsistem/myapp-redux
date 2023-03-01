bb9981353c728a32ba09b7bc4e74aceda6edee51
John <58703369+johnsistem@users.noreply.github.com>
2023-02-28 18:53:13 -0600

    este cambio es de nueva-rama

--- pages/index.js
+++ pages/index.js
@@ -38,58 +38,8 @@ const usersList = useSelector((state) => state.usersData);
 
 	console.log("lista de usuarios:",users);
 	return (
-		<div className={styles.container}>
-			<main className={styles.main}>
-				<div className={styles.grid}>
-					<Link href="123">
-						<a href="https://nextjs.org/docs" className={styles.card}>
-							<h2>Documentation &rarr;</h2>
-							<p>
-								{/* cambios en main */}
-								este es mi cambio 2 
-							</p>
-						</a>
-					</Link>
-				</div>
-				<div className={styles.grid}>
-					<a href="./contact" className={styles.card}>
-						<h2>NEW PAGE &rarr;</h2>
-						<p>
-							{/* Find in-depth information about Next.js features and API. */}
-						</p>
-					</a>
-				</div>
-				<div>
-					{users.map((user) => (
-						
-						<div key={user.id}>
-							<Link href={`/${user.id}`} passHref>
-
-								{user.username}
-							</Link>
-								</div>
-						
-					))}
-				</div>
-			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{" "}
-					<span className={styles.logo}>
-						<Image
-							src="/vercel.svg"
-							alt="Vercel Logo"
-							width={72}
-							height={16}
-						/>
-					</span>
-				</a>
-			</footer>
+		<div>
+			esto es de la nuevo-rama
 		</div>
 	);
 }
